cc57cc7ae61105d44a96e6e4e698997dbf369b4f
Smbat <sampogosyan1995@gmail.com>
2019-01-21 17:41:47 +0400

    Updates

---
+++ precache-manifest.e1a1874ce9d10067e7b6abbd32c2f15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "975116613ea77cd802a3",
+    "url": "/todolist/static/css/main.905da3c2.chunk.css"
+  },
+  {
+    "revision": "975116613ea77cd802a3",
+    "url": "/todolist/static/js/main.97511661.chunk.js"
+  },
+  {
+    "revision": "dc1719aeec3215eed9e6",
+    "url": "/todolist/static/css/1.d5c555b2.chunk.css"
+  },
+  {
+    "revision": "dc1719aeec3215eed9e6",
+    "url": "/todolist/static/js/1.dc1719ae.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "41e71f38ba31d79a8b7e0aff6db40ac9",
+    "url": "/todolist/index.html"
+  }
+];
